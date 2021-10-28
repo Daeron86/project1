932e557b292737492b88b90118d3ed3f17529c73
Ivan <deevserg3@gmail.com>
2021-10-28 15:38:54 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+a = 5;
+console.log(a)
+//alert("Hello world!")
+
+//let answer = confirm("Ты тут?");
+
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof(answer));
+
+//console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
